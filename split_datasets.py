0db7f59ac67b02bf45e814bfd9df9b04c927b7c2
Miguel Caballero <caballero.mc98@gmail.com>
2020-07-06 18:25:10 -0500

    added validation to split_datasets.py

--- split_datasets.py
+++ split_datasets.py
@@ -2,13 +2,15 @@ from shutil import move
 import os
 from random import shuffle
 
-TARGET_PATH = '1d_barcode_extended_plain/1d_barcode_extended_plain/Detection'
-PREDICTOR_PATH = '1d_barcode_extended_plain/1d_barcode_extended_plain/Original'
+MASK_PATH = '1d_barcode_extended_plain/1d_barcode_extended_plain/Detection'
+IMAGE_PATH = '1d_barcode_extended_plain/1d_barcode_extended_plain/Original'
 TRAIN_PATH = 'dataset/train'
 TEST_PATH = 'dataset/test'
+VALIDATION_PATH = 'dataset/validation'
 
-target_path_list_dir = os.listdir(TARGET_PATH)
-TRAIN_SIZE = .7
+mask_path_list_dir = os.listdir(MASK_PATH)
+TEST_SIZE = .2
+VALIDATION_SIZE = .2
 
 
 class Path:
@@ -28,27 +30,30 @@ class Path:
 if __name__ == '__main__':
     Path.create_directory(TRAIN_PATH)
     Path.create_directory(TEST_PATH)
-    shuffle(target_path_list_dir)
-    target_path_length = len(target_path_list_dir)
-    train_length = round(target_path_length * TRAIN_SIZE)
-    test_length = round(target_path_length * (1 - TRAIN_SIZE))
-    paths_list = [TRAIN_PATH, TEST_PATH]
-    train_index = [0, train_length]
-    test_index = [train_length, train_length + test_length]
-    index_list = [train_index, test_index]
+    Path.create_directory(VALIDATION_PATH)
+    shuffle(mask_path_list_dir)
+    mask_path_length = len(mask_path_list_dir)
+    train_length = round(mask_path_length * (1 - TEST_SIZE))
+    test_length = round(mask_path_length * TEST_SIZE)
+    validation_length = round(train_length * VALIDATION_SIZE)
+    train_length = round(train_length * (1 - VALIDATION_SIZE))
+
+    paths_list = [TRAIN_PATH, TEST_PATH, VALIDATION_PATH]
+    test_index = [0, test_length]
+    train_index = [test_length, test_length + train_length]
+    validation_index = [test_length + train_length, test_length + validation_length + train_length]
+    index_list = [train_index, test_index, validation_index]
     for index, datatype_path in zip(index_list, paths_list):
         for i in range(index[0], index[1]):
-            filename = target_path_list_dir[i]
+            filename = mask_path_list_dir[i]
+            mask_src_file_path = os.path.join(MASK_PATH, filename)
+            mask_dst_path = os.path.join(datatype_path, 'mask')
+            Path.create_directory(mask_dst_path)
+            mask_dst_path = os.path.join(mask_dst_path, filename)
+            move(mask_src_file_path, mask_dst_path)
 
-            target_src_file_path = os.path.join(TARGET_PATH, filename)
-            target_dst_path = os.path.join(datatype_path, 'target')
-            Path.create_directory(target_dst_path)
-            target_dst_path = os.path.join(target_dst_path, filename)
-            move(target_src_file_path, target_dst_path)
-
-            predictor_src_file_path = os.path.join(PREDICTOR_PATH, filename)
-            predictor_dst_path = os.path.join(datatype_path, 'predictor')
-            Path.create_directory(predictor_dst_path)
-            predictor_dst_path = os.path.join(predictor_dst_path, filename)
-            move(predictor_src_file_path, predictor_dst_path)
-
+            image_src_file_path = os.path.join(IMAGE_PATH, filename)
+            image_dst_path = os.path.join(datatype_path, 'image')
+            Path.create_directory(image_dst_path)
+            image_dst_path = os.path.join(image_dst_path, filename)
+            move(image_src_file_path, image_dst_path)
